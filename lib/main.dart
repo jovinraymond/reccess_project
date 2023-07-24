2e3205f34f3f184f57b7ced4b2789104cf2c3286
Namutosi Dianah <dianah.namutosi10@gmail.com>
2023-07-24 19:27:45 +0300

    added login and edited main.dart and pubspec file

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,32 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+    show MaterialApp, WidgetsFlutterBinding, runApp;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:loginuicolors/message.dart';
+import 'home.dart';
+import 'login.dart';
+import 'register.dart';
+import 'message.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyDMyNfjfT02ENgSIvoHAo7ZWr7CZXouK0s",
+          authDomain: "tenant-ms-2eba0.firebaseapp.com",
+          projectId: "tenant-ms-2eba0",
+          storageBucket: "tenant-ms-2eba0.appspot.com",
+          messagingSenderId: "54381844554",
+          appId: "1:54381844554:web:eddf986220d57dd2292eb3",
+          measurementId: "G-16FXFJM7EQ"));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'message': (context) => ChatScreen(),
+      'home': (context) => Homep(),
+      'login': (context) => MyLogin(),
+    },
+  ));
 }
