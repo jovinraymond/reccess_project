6f8519225f812dc2576b371f421dede3ef8bc4a5
jovinraymaond <jovinkyomugaso@gmail.com>
2023-07-25 09:48:06 +0300

    i added the register and the home page

---
+++ lib/home.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'payment_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyLogin(),
+  ));
+}
+
+class Homep extends StatefulWidget {
+  const Homep({Key? key}) : super(key: key);
+
+  @override
+  State<Homep> createState() => _HomepState();
+}
+
+class _HomepState extends State<Homep> {
+  String _searchQuery = '';
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Tenant Management system"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: [
+              //TextField(autofillHints: ,)
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 50, left: 40, right: 45, bottom: 30),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(23),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage("assets/ava1.jpg"),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  "Your Name",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.amber,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //SizedBox(
+          //height: 30,
+          //),
+          Container(
+            width: 400,
+            height: 250,
+            padding: EdgeInsets.only(top: 50, left: 50, right: 45, bottom: 30),
+            decoration: BoxDecoration(
+              color: Colors.blue[300],
+              borderRadius: BorderRadius.circular(23),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      color: Colors.green,
+                      //decoration: BoxDecoration(
+                      //borderRadius: BorderRadius.circular(10)),
+                      child: MaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PaymentPage()),
+                          );
+                        },
+                        child: Text("Payment"),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      color: Colors.amber,
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: Text("Maintenance"),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(7),
+                      color: Colors.purple,
+                      child: MaterialButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "message");
+                        },
+                        child: Text("Message"),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(9),
+                      color: Color(0xffff0713),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.home),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MyRegister extends StatefulWidget {
+  const MyRegister({Key? key}) : super(key: key);
+
+  @override
+  _MyRegisterState createState() => _MyRegisterState();
+}
+
+class _MyRegisterState extends State<MyRegister> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // Form is valid, save the user data to Cloud Firestore
+      String email = emailController.text;
+      String name = nameController.text;
+      String password = passwordController.text;
+      String phone = phoneController.text;
+
+      // Add user data to Firestore
+      FirebaseFirestore.instance.collection('users').add({
+        'email': email,
+        'name': name,
+        'password': password,
+        'phone': phone,
+      }).then((value) {
+        // Data added successfully
+        print('User data added to Firestore!');
+        // You can perform additional actions here
+      }).catchError((error) {
+        // Error occurred while adding data
+        print('Error adding user data to Firestore: $error');
+        // You can handle the error gracefully here
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/pihc.jpg'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Stack(children: [
+          Container(
+            padding: EdgeInsets.only(left: 30, top: 20),
+            child: Text(
+              'Create Your Account',
+              style: TextStyle(color: Colors.amber, fontSize: 33),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.28),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          style: TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ),
+                            ),
+                            hintText: "Name",
+                            hintStyle: TextStyle(color: Colors.black),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          controller: nameController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your name';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ),
+                            ),
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.black),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          controller: emailController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ),
+                            ),
+                            hintText: "Phone Number",
+                            hintStyle: TextStyle(color: Colors.black),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          controller: phoneController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your phone number';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.black),
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ),
+                            ),
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.black),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          controller: passwordController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                _submitForm();
+                                Navigator.pushNamed(context, 'login');
+                              },
+                              child: Text(
+                                "Sign up",
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Color(0xfff5f6f9),
+                                  fontSize: 25,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        // Add more TextFormFields or other form fields here
+                        // ...
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
