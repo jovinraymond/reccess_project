80122cbe55169568d10e7330489c638fc28aa387
jovinraymaond <jovinkyomugaso@gmail.com>
2023-07-26 16:50:28 +0300

    i have added the payment page

---
+++ lib/payment_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mobile Money Payment'),
+      ),
+      body: Center(
+        child: Text('This is the mobile money payment page.'),
+      ),
+    );
+  }
+}
